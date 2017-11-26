3ac99450610a38f9d71a0d0bcfb407ca1dd19f04
Nolan Byrnes <nolanehs@gmail.com>
2017-11-26 13:24:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,12 @@ require_relative "./user.rb"
 
 class Student < User
 
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :knowledge
 
   def initialize
     @first_name = first
     @last_name = last
+    @knowledge = []
   end
 
 
