6313838e81df573223d884fb267550592c361251
Nolan Byrnes <nolanehs@gmail.com>
2017-11-26 13:29:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,11 @@ class Student < User
 
   attr_accessor :first_name, :last_name, :knowledge
 
-
+  def initialize
+    @first_name = first
+    @last_name = last
+    @knowledge = []
+  end
 
 def learn(string)
 @knowledge << string
